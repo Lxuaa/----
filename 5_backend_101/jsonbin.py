5667dad1198e7bd2879f2e95d9814536ecc7fe56
Xuanhao Liang <3293864428@qq.com>
2025-12-10 11:27:02 +0800

    1

--- 5_backend_101/jsonbin.py
+++ 5_backend_101/jsonbin.py
@@ -1,7 +1,7 @@
 import requests
 from datetime import datetime
 
-JSONBIN_BIN_ID = "6938125643b1c97be9e1cb59"
+JSONBIN_BIN_ID = "6938e853ae596e708f8f394a"
 JSONBIN_ACCESS_KEY = "$2a$10$f5JQy37tXAMbfUTBjStTWeR7rLbgELpDBbGXBDXj3RCo9EsZoGxB."
 
 JSONBIN_URL = f"https://api.jsonbin.io/v3/b/{JSONBIN_BIN_ID}"
